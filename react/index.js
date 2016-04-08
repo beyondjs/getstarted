649a0464218f1fd8b936d59383e178201f0ed807
Enrique Box <ebox@socites.com>
2016-04-08 12:19:08 -0300

    Initial setup

--- react/index.js
+++ react/index.js
@@ -1,14 +1,28 @@
 function Page() {
     "use strict";
 
-    var $container;
+    var $container, container;
 
     // shortcuts to react objects
     var React = module.React;
     var ReactDOM = module.ReactDOM;
 
+    var model = new Model();
+
     function render() {
 
+        $container = $('<div/>')
+            .attr('id', "react-hello-container");
+
+        $('body').append($container);
+        container = $('body').find('#react-hello-container').get(0);
+
+        var HelloReactComponent = react.hello;
+        ReactDOM.render(
+            React.createElement(HelloReactComponent, model),
+            container);
+
+        if (done) done();
 
     }
 
